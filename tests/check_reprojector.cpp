437305b64933df618be024e5ddacb95d11a49ffa
David Matthew Mattli <dmm@mattli.us>
2011-01-26 21:02:23 -0600

    Two more tests for Reprojector

--- tests/check_reprojector.cpp
+++ tests/check_reprojector.cpp
@@ -18,15 +18,32 @@ bool extents_are_continuous(vector<ChunkExtent> extents, long row_count)
 
 	std::sort(extents.begin(), extents.end());
 
+	for (int i = 0; i < extents.size()-1; ++i) {
+		if (extents[i].lastIndex() +1 != extents[i+1].firstIndex()) {
+			fprintf(stderr, "Discontinuity at index: %ld, values: %ld %ld\n",
+				i, extents[i].lastIndex()+1, extents[i+1].firstIndex());
+			return false;
+		}
+	}
+
 	return true;
 }
 
-TEST(reprojector_test, extents_basic_tests) {
+TEST(reprojector_test, extents_correct_max_value) {
+	vector<ChunkExtent> ce = Reprojector::getChunkExtents(10000, 100, 10);
+	
+	std::sort(ce.begin(), ce.end());
+
+	ASSERT_EQ(10000 - 1, ce.back().lastIndex());
+
+}
+
+TEST(reprojector_test, extents_continuity) {
 
 	vector<ChunkExtent> ce = Reprojector::getChunkExtents(10000, 100, 10);
 
 
-
+	ASSERT_EQ(10000 - 1, ce.back().lastIndex());
 	ASSERT_TRUE(extents_are_continuous(ce, 10000));
 
 }
