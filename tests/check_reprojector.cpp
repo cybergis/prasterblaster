7db8ee75be8b8f318506b22b2fc46f59e66b0683
David Matthew Mattli <dmm@mattli.us>
2011-01-31 14:07:14 -0600

    Wider scope of testing

--- tests/check_reprojector.cpp
+++ tests/check_reprojector.cpp
@@ -30,11 +30,20 @@ bool extents_are_continuous(vector<ChunkExtent> extents, long row_count)
 }
 
 TEST(reprojector_test, extents_correct_max_value) {
-	vector<ChunkExtent> ce = Reprojector::getChunkExtents(10000, 100, 10);
 	
-	std::sort(ce.begin(), ce.end());
+	for (int i = 1000; i < 20000; i+=1000) {
+		for (int j=1; j < 500; j+=10) {
+			for (int k=5; k < j*2; k+=10) {
+				vector<ChunkExtent> ce = Reprojector::getChunkExtents(i, k, j);
+				std::sort(ce.begin(), ce.end());
+				ASSERT_EQ(i - 1, ce.back().lastIndex());
+				
+			}
+		}
+		
+	}
 
-	ASSERT_EQ(10000 - 1, ce.back().lastIndex());
+
 
 }
 
