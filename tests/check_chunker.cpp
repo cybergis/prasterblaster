e0156aee0673feb391819f6c2cf6a9b893b85f56
David Matthew Mattli <dmattli@usgs.gov>
2011-06-07 08:40:38 -0500

    Add Fixture test and fix fixture

--- tests/check_chunker.cpp
+++ tests/check_chunker.cpp
@@ -23,8 +23,13 @@ protected:
 		bool result = false;
 		in = shared_ptr<ProjectedRaster>(new ProjectedRaster(test_dir + "glc_geographic_30sec.img"));
 		shared_ptr<Projection> proj = shared_ptr<Projection>(Transformer::convertProjection(MOLL));
+		proj->setUnits(in->getProjection()->units());
+		proj->setDatum(in->getProjection()->datum());
 		proj->setParams(in->getProjection()->params());
+		ProjectedRaster::CreateRaster(test_dir + "glc_mollweide_30sec-chunk.img",
+					      in, proj, in->getPixelType(), in->getPixelSize());
 		out = shared_ptr<ProjectedRaster>(new ProjectedRaster(test_dir + "glc_mollweide_30sec-chunk.img"));
+
 	}
 	
 protected:
@@ -32,6 +37,23 @@ protected:
 	shared_ptr<ProjectedRaster> out;
 };
 
+TEST_F(ChunkerTest, check_fixture) {
+	ASSERT_TRUE(out->isReady());
+
+
+}
+
+TEST_F(ChunkerTest, check_chunk_sizes) {
+	Chunker c(in, out);
+	vector<ChunkExtent> chunks;
+	ASSERT_NO_THROW ({
+			chunks = c.getChunksByCount(1, 0);
+		});
+
+	ASSERT_NE(chunks.size(), 0);
+	ASSERT_EQ(chunks.back().lastIndex+1, out->getRowCount());
+	
+}
 
 TEST_F(ChunkerTest, chunk_output_comprehensive) {
 	
