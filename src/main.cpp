839171ffd5fb4b07244dd000b8d27c9591dbd9df
David Matthew Mattli <dmm@mattli.us>
2011-06-13 15:01:25 -0500

    Add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+/*
+  Programmer: David Mattli
+*/
+
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+
+#include <mpi.h>
+
+#include "gctp_cpp/projection.h"
+#include "gctp_cpp/transformer.h"
+#include "gctp_cpp/mollweide.h"
+#include "projectedraster.hh"
+#include "reprojector.hh"
+#include "rasterreader.hh"
+
+#include <gdal_priv.h>
+
+#include "driver.hh"
+
+const char *usage = "usage: prasterblaster <input raster path> <output raster path> " 
+  "<output raster projection> \n"; 
+
+
+int main(int argc, char *argv[]) 
+{
+ 	long int rows, cols;
+
+	ProjectedRaster *out;
+	Reprojector *re = 0;
+	int rank;
+
+        rows = cols = 0;
+
+	MPI_Init(&argc, &argv);
+	if (argc < 4) {
+		if (rank == 0) {
+			printf("%s\n", usage);
+		}
+		MPI_Abort(MPI_COMM_WORLD, -1);
+		return 0;
+        }
+
+	driver(argv[1], argv[2], argv[3]);
+	MPI_Finalize();
+	
+	return 0;
+}
