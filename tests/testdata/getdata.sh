e99ec8f48420d0a9cce3f62399c97ff004b3da9e
David Matthew Mattli <dmm@mattli.us>
2011-06-27 17:17:38 -0500

    Add raster fetch script

---
+++ tests/testdata/getdata.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget ftp://ftpext.usgs.gov/pub/cr/mo/rolla/carto_research/projections/input_files/Global_Vegetation/veg_geographic_1deg.img
+wget ftp://ftpext.usgs.gov/pub/cr/mo/rolla/carto_research/projections/input_files/Global_Vegetation/veg_geographic_1deg.xml
+wget ftp://ftpext.usgs.gov/pub/cr/mo/rolla/carto_research/projections/input_files/Holdridge_Life_Zones/holdnorm_geographic_30min.img
+wget ftp://ftpext.usgs.gov/pub/cr/mo/rolla/carto_research/projections/input_files/Holdridge_Life_Zones/holdnorm_geographic_30min.xml
+wget ftp://ftpext.usgs.gov/pub/cr/mo/rolla/carto_research/projections/input_files/Global_Land_Cover/glc_geographic_30sec.img
+wget ftp://ftpext.usgs.gov/pub/cr/mo/rolla/carto_research/projections/input_files/Global_Land_Cover/glc_geographic_30sec.xml
