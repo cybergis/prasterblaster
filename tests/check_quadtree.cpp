19d22e47285d15d4ae71554c7f852d7030d40df7
David Matthew Mattli <dmattli@usgs.gov>
2012-08-06 11:55:16 -0500

    Convert QuadTree class to use an explicit stack to avoid stack overflow during recursion

--- tests/check_quadtree.cpp
+++ tests/check_quadtree.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 
 #include <memory>
+#include <cstdlib>
 
 #include "projectedraster.hh"
 #include "sharedptr.hh"
@@ -10,12 +11,15 @@
 TEST(QUADTREE, quadtree_init) 
 {
 	Area a(0, 99, 99, 0);
-	QuadTree tree(a, 25*25);
+	QuadTree tree(a, 50*50);
 
 	vector<Area> leaves = tree.collectLeaves();
-
+	printf("%zd\n", leaves.size());
 	for (int i = 0; i < leaves.size(); ++i) {
-
+		Area t = leaves.at(i);
+		printf("Node: %d {%f %f %f %f} \n",
+		       i,
+		       t.ul.x, t.ul.y, t.lr.x, t.lr.y);
 	}
 
 	return;
