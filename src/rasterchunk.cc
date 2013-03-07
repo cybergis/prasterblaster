25e8877cb26bb7cbfd734f3f66afb247f5a89b45
David Matthew Mattli <dmattli@usgs.gov>
2013-03-07 08:29:07 -0600

    Add copyright header to rasterchunk.cc

--- src/rasterchunk.cc
+++ src/rasterchunk.cc
@@ -1,3 +1,20 @@
+//
+// Copyright 0000 <Nobody>
+// @file
+// @author David Matthew Mattli <dmattli@usgs.gov>
+//
+// @section LICENSE
+//
+// This software is in the public domain, furnished "as is", without
+// technical support, and with no warranty, express or implied, as to
+// its usefulness for any purpose.
+//
+// @section DESCRIPTION
+//
+// The RasterChunk class represents an in-memory, georeferenced section of
+// raster.
+//
+//
 
 #include <gdal.h>
 
