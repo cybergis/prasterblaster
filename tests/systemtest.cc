cf0ffd952d508f9fe52e32afdec2075919f99cf1
David Matthew Mattli <dmattli@usgs.gov>
2013-03-07 15:05:01 -0600

    Add the rest of the rasters to the systemtest

--- tests/systemtest.cc
+++ tests/systemtest.cc
@@ -18,15 +18,21 @@ int main(int argc, char *argv[]) {
   const int raster_count = 2;
   const char *input_files[] = { 
     "tests/testdata/glc_geographic_30sec.tif",
+    "tests/testdata/veg_geographic_1deg.tif",
+    "tests/testdata/holdnorm_geographic_30min.tif",
     "tests/testdata/nlcd2006_landcover_4-20-11_se5.tif" 
   };
 
   const char *output_files[] = {
     "tests/testoutput/glc_mollweide_30sec.tif",
+    "tests/testoutput/veg_geographic_1deg.tif",
+    "tests/testoutput/holdnorm_geographic_30min.tif",
     "tests/testoutput/nlcd2006_landcover_4-20-11_se5_mollweide.tif"
   };
 
   const char *output_srs[] = {
+    "+proj=moll +a=6370997 +b=6370997",
+    "+proj=moll +a=6370997 +b=6370997",
     "+proj=moll +a=6370997 +b=6370997",
     "+proj=moll +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83"
   };
@@ -47,7 +53,7 @@ int main(int argc, char *argv[]) {
   conf.resampler = librasterblaster::MIN;
   
   int ret = 0;
-  for (int i=1; i<raster_count; ++i) {
+  for (int i=0; i<raster_count; ++i) {
     conf.input_filename = input_files[i];
     conf.output_filename = output_files[i];
     conf.output_srs = output_srs[i];
