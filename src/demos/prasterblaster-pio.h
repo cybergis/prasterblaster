b530d987d9c8a10be380714f0782af3ce6eb6bf5
David Matthew Mattli <dmattli@usgs.gov>
2013-07-29 16:28:41 -0500

    Add include guard to prasterblaster-pio.h

--- src/demos/prasterblaster-pio.h
+++ src/demos/prasterblaster-pio.h
@@ -1,6 +1,28 @@
+/*!
+ * Copyright 0000 <Nobody>
+ * @file
+ * @author David Matthew Mattli <dmattli@usgs.gov>
+ *
+ * @section LICENSE
+ *
+ * This software is in the public domain, furnished "as is", without
+ * technical support, and with no warranty, express or implied, as to
+ * its usefulness for any purpose.
+ *
+ * @section DESCRIPTION
+ *
+ * Header for high-level API
+ *
+ */
+#ifndef SRC_DEMOS_PRASTERBLASTER_PIO_H_
+#define SRC_DEMOS_PRASTERBLASTER_PIO_H_
+
 #include "src/configuration.h"
 #include "src/utils.h"
 
 namespace librasterblaster {
 PRB_ERROR prasterblasterpio(librasterblaster::Configuration conf);
 }
+
+#endif  // SRC_DEMOS_PRASTERBLASTER_PIO_H_
+
