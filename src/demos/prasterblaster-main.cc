59a79f0801161a2770039edc99c2efa364fa78d3
David Matthew Mattli <dmattli@usgs.gov>
2013-07-29 13:54:55 -0500

    Add copyright message and remove bad whitespace.

--- src/demos/prasterblaster-main.cc
+++ src/demos/prasterblaster-main.cc
@@ -1,20 +1,37 @@
+//
+// Copyright 0000 <Nobody>
+// @file
+// @author David Matthew Mattli <dmattli@usgs.gov>
+//
+// @section LICENSE
+//
+// This software is in the public domain, furnished "as is", without
+// technical support, and with no warranty, express or implied, as to
+// its usefulness for any purpose.
+//
+// @section DESCRIPTION
+//
+// The Configuration class represents the configuration of a reprojection task.
+//
+//
 
 #include <mpi.h>
 
 #include "src/configuration.h"
 #include "src/demos/prasterblaster-pio.h"
 
-using namespace librasterblaster;
+using librasterblaster::Configuration;
+using librasterblaster::prasterblasterpio;
 
 int main(int argc, char *argv[]) {
   int rank = 0;
   int process_count = 0;
   RasterChunk *in_chunk, *out_chunk;
-  
+
   // Give MPI_Init first run at the command-line arguments
   MPI_Init(&argc, &argv);
-  
-  // Initialize Configuration object 
+
+  // Initialize Configuration object
   Configuration conf(argc, argv);
 
   int ret =  prasterblasterpio(conf);
